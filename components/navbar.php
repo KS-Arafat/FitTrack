59c83f20b9e48dc1452f42d06610ca3f08092d0f
KS-Arafat <sfnarafat@gmail.com>
2025-03-18 16:34:19 +0600

    PHP Comps Added

---
+++ components/navbar.php
@@ -0,0 +1,29 @@
+<nav>
+    <div class="nav__bar">
+        <div class="nav__header">
+            <div class="nav__logo">
+                <a class="logo__link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="lucide-ico">
+                        <path
+                            d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
+                        <path d="M12 7v6" />
+                        <path d="M9 10h6" />
+                    </svg>
+                </a>
+                <p class="logo__text">FitTracker</p>
+            </div>
+            <div class="nav__menu__btn" id="menu-btn">
+                <i class="ri-menu-line"></i>
+            </div>
+        </div>
+        <ul class="nav__links" id="nav-links">
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="about.php">ABOUT</a></li>
+            <li><a href="features.php">FEATURES</a></li>
+            <li><a href="clients.php">CLIENT</a></li>
+            <li><a href="blogs.php">BLOG</a></li>
+            <li><a href="contact.php">CONTACT US</a></li>
+        </ul>
+    </div>
+</nav>
